9f4a586151e59a32d1fd2017a45b0fb0b644b0b4
manantsoa <manantsoa.razana@gmail.com>
2015-04-11 13:39:29 +0200

    Unit tests of DemandeVisite class

--- grails-app/domain/toulousemusee/DemandeVisite.groovy
+++ grails-app/domain/toulousemusee/DemandeVisite.groovy
@@ -15,6 +15,18 @@ class DemandeVisite {
     static constraints = {
         code blank: false
         statut blank: false
+        musees nullable: true
+        debutPeriode min: new Date(), nullable: false, validator: {value, reference ->
+            if (reference.finPeriode == null)
+                return false
+            else
+                return value.before(reference.finPeriode)}
+        finPeriode nullable: false, validator: {value, reference ->
+            if (reference.debutPeriode == null)
+                return false
+            else
+                return value.after(reference.debutPeriode)}
+        nbPersonnes max: 6, min: 1
     }
 
     static mapping = {
--- test/unit/toulousemusee/DemandeVisiteSpec.groovy
+++ test/unit/toulousemusee/DemandeVisiteSpec.groovy
@@ -9,12 +9,36 @@ import spock.lang.Specification
 @TestFor(DemandeVisite)
 class DemandeVisiteSpec extends Specification {
 
-    def setup() {
+    void "la validité d'une demande de visite valide"(String unCode, Date unDebut
+    , Date uneFin, int unNbP, String unStatut) {
+        given: "un nom, un début, une fin, un nombre de personnes et un statut correctement initialisés"
+        DemandeVisite uneDemande = new DemandeVisite(code: unCode, debutPeriode: unDebut,
+                        finPeriode: uneFin, nbPersonnes: unNbP, statut: unStatut)
+
+        expect: "la demande de visite est valide"
+        uneDemande.validate() == true
+
+        where:
+        unCode|unDebut|uneFin|unNbP|unStatut
+        "un code"|new Date(2016,10,20)|new Date(2016,10,30)|5|"un statut"
     }
 
-    def cleanup() {
-    }
+    void "l'invalidité d'une demande de visite"(String unCode, Date unDebut,
+                                                Date uneFin, int unNbP, String unStatut) {
+        given: "un nom, un début, une fin, un nombre de personnes et un statut mal initialisés"
+        DemandeVisite uneDemande = new DemandeVisite(code: unCode, debutPeriode: unDebut,
+                finPeriode: uneFin, nbPersonnes: unNbP, statut: unStatut)
 
-    void "test something"() {
+        expect: "la demande de visite est invalide"
+        uneDemande.validate() == false
+
+        where:
+        unCode|unDebut|uneFin|unNbP|unStatut
+        null|new Date()|new Date()|5|"un statut"
+        "un code"|null|new Date()|5|"un statut"
+        "un code"|new Date()|null|5|"un statut"
+        "un code"|new Date()|new Date()|5|null
+        "un code"|new Date(2016,10,10)|new Date(2009,9,9)|5|"un statut"
+        "un code"|new Date()|new Date()|8|"un statut"
     }
 }
